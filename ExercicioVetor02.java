a55e56858a9aed3c8fbd4c98446ec7bc5ed33967
Leonardo Moreira da Silva <leonardomoreira050@gmail.com>
2018-05-04 08:16:19 -0300

    Exercicio Vetores 02

--- ExercicioVetor02.java
+++ ExercicioVetor02.java
@@ -1,5 +1,18 @@
 public class ExercicioVetor02{
     public static void main(String[] args){
+
+        String nome[] = new String[10];
+        nome[0] = "Leonardo Moreira Stephaniakwe da Silva"; 
+        nome[1] = "Leonardo Moreira Stephaniakwe da Silva";
+        nome[2] = "Leonardo Moreira Stephaniakwe da Silva";
+        nome[3] = "Leonardo Moreira Stephaniakwe da Silva";
+        nome[4] = "Leonardo Moreira Stephaniakwe da Silva";
+        nome[5] = "Leonardo Moreira Stephaniakwe da Silva";
+        nome[6] = "Leonardo Moreira Stephaniakwe da Silva";
+        nome[7] = "Leonardo Moreira Stephaniakwe da Silva";
+        nome[8] = "Leonardo Moreira Stephaniakwe da Silva";
+        nome[9] = "Leonardo Moreira Stephaniakwe da Silva";
+        System.out.println("Lista: " + "\n"+ nome[0] + "\n" +nome[1] + "\n" +nome[3]);
         
     }
 }
